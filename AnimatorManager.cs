0d628265771fdcf9531c3178760743e64c9e6017
Maksim Kuznetsov <m_kuznetsov@kbtu.kz>
2023-08-17 01:50:15 +0600

    init

---
+++ AnimatorManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    Animator animator;
+    int horizontalId;
+    int verticalId;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        horizontalId = Animator.StringToHash("Horizontal");
+        verticalId = Animator.StringToHash("Vertical");
+    }
+
+    public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
+    {
+        // Animation Snapping
+        float snappedHorizontal;
+        float snappedVertical;
+
+        #region Snapped Horizontal
+        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+        {
+            snappedHorizontal = 0.5f;
+        }
+        else if (horizontalMovement > 0.55f)
+        {
+            snappedHorizontal = 1;
+        } else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+        {
+            snappedHorizontal = -0.5f;
+        } else if (horizontalMovement < -0.55f)
+        {
+            snappedHorizontal = -1;
+        } else
+        {
+            snappedHorizontal = 0;
+        }
+        #endregion
+        #region Snapped Vertical
+        if (verticalMovement > 0 && verticalMovement < 0.55f)
+        {
+            snappedVertical = 0.5f;
+        }
+        else if (verticalMovement > 0.55f)
+        {
+            snappedVertical = 1;
+        }
+        else if (verticalMovement < 0 && verticalMovement > -0.55f)
+        {
+            snappedVertical = -0.5f;
+        }
+        else if (verticalMovement < -0.55f)
+        {
+            snappedVertical = -1;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+        #endregion
+
+        animator.SetFloat(horizontalId, snappedHorizontal, 0.1f, Time.deltaTime);
+        animator.SetFloat(verticalId, snappedVertical, 0.1f, Time.deltaTime);
+    }
+}
